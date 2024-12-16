273eb0f8f7a2b900cffa657d1191a5f6fe0edd68
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-16 15:01:26 +0100

    people.js import

---
+++ people.js
@@ -0,0 +1,18 @@
+// Importa le funzioni dai file names.js e hobbies.js
+const name = require('./names');
+const hobbies = require('./hobbies');
+
+// Funzione che non accetta parametri
+function person() {
+    // Uso name per ottenere il nome completo
+    const fullName = name('Mario', 'Rossi');
+    
+    // Uso createHobbies per ottenere gli hobby
+    const hobbies = hobbies('calcio', 'lettura', 'viaggi');
+    
+    // Costruisco l'oggetto finale
+    return {
+        fullName: `${fullName.firstName} ${fullName.lastName}`,
+        hobbies: hobbies.hobbies
+    };
+}
